42059cdf2a1ab59aa0a4e597d0f81d197f340a04
FerbaDev <elferbarron1@gmail.com>
2023-08-29 00:12:40 -0300

    se configura backend

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -75,7 +75,7 @@ const Checkout = () => {
     });
     try {
       let response = await axios.post(
-        "http://localhost:8080/create_preference",
+        "https://backend-cactivas.vercel.app/create_preference",
         {
           items: newArray,
           shipment_cost: shipmentCost,
