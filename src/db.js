e190c70f87c0271328f0734a309ec0132fd399fa
FerbaDev <elferbarron1@gmail.com>
2023-08-28 23:22:12 -0300

    trabajo realizado, variables de entorno, listo para guardar

---
+++ src/db.js
@@ -0,0 +1,146 @@
+export const products = [
+  {
+    title: "Crassula hemisphaerica",
+    description: "Maceta 8",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 600,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104496/catalogo%20cactivas/cat1_gcvhy9.jpg",
+  },
+  {
+    title: "A. Reina Victoria",
+    description: "Bols 12",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 2300,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104496/catalogo%20cactivas/cat16_wi8hyc.jpg",
+  },
+  {
+    title: "S. Golden Hanni",
+    description: "Bols 12",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 1300,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104496/catalogo%20cactivas/cat2_qplfh0.jpg",
+  },
+  {
+    title: "G. Debbie",
+    description: "Maceta 8",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 1300,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104495/catalogo%20cactivas/cat14_hcbucb.jpg",
+  },
+  {
+    title: "G. Debbie",
+    description: "Maceta 12",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 2500,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104496/catalogo%20cactivas/cat15_nbmnmi.jpg",
+  },
+  {
+    title: "Gv. Bashfull",
+    description: "Maceta 8",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 1000,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104495/catalogo%20cactivas/cat11_sx8nbl.jpg",
+  },
+  {
+    title: "A. filifera",
+    description: "Bols 12",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 900,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104495/catalogo%20cactivas/cat10_kd29nv.jpg",
+  },
+  {
+    title: "E. chihuahuaensis",
+    description: "Maceta 8",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 1800,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104495/catalogo%20cactivas/cat13_enflqd.jpg",
+  },
+  {
+    title: "E. peacocki",
+    description: "Maceta 8",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 1700,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104495/catalogo%20cactivas/cat12_aioa4v.jpg",
+  },
+  {
+    title: "Greenovia d",
+    description: "Maceta 8",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 500,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104495/catalogo%20cactivas/cat9_xufz8n.jpg",
+  },
+  {
+    title: "P. buiningii",
+    description: "Maceta 8",
+    category: "cactus",
+    stock: 2,
+    unit_price: 1800,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104494/catalogo%20cactivas/cat4_zl5nmg.jpg",
+  },
+  {
+    title: "A. maculatus",
+    description: "Maceta 8",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 1300,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104494/catalogo%20cactivas/cat3_pt5c2e.jpg",
+  },
+  {
+    title: "E. setosa",
+    description: "Maceta 6",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 100,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104494/catalogo%20cactivas/cat5_gukopw.jpg",
+  },
+  {
+    title: "Aeonium n",
+    description: "Maceta 9",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 600,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104494/catalogo%20cactivas/cat7_oltnnw.jpg",
+  },
+  {
+    title: "Cerebrito",
+    description: "Maceta 8",
+    category: "cactus",
+    stock: 2,
+    unit_price: 1800,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104494/catalogo%20cactivas/cat8_uwy6xj.jpg",
+  },
+  {
+    title: "E. nodulosa",
+    description: "Maceta 8",
+    category: "suculentas",
+    stock: 2,
+    unit_price: 800,
+    image:
+      "https://res.cloudinary.com/dwl4yy7q6/image/upload/v1692104494/catalogo%20cactivas/cat6_sxhu5z.jpg",
+  },
+];
